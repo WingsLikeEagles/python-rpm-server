0ff4507cb5d6c9c822b750a216b04fe344286e1c
WingsLikeEagles <40436741+WingsLikeEagles@users.noreply.github.com>
2020-11-03 09:44:47 -0800

    Create rpm-server.py
    How insecure is this?

---
+++ rpm-server.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from os import chdir
+from http.server import HTTPServer, CGIHTTPRequestHandler
+# this is the folder where you need to store your files to be served.
+files='./data'
+chdir(files)
+# This is the port you want to listen for requests
+port=8083
+server_object = HTTPServer(server_address=('',port),
+RequestHandlerClass=CGIHTTPRequestHandler)
+print('Starting server on port ', port)
+server_object.serve_forever()
