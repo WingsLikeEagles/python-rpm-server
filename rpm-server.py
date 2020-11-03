78d980e048d9e193c0f33b1eec19373e299a0323
WingsLikeEagles <40436741+WingsLikeEagles@users.noreply.github.com>
2020-11-03 09:47:49 -0800

    Update rpm-server.py
    Changed port to 80, directory name to files

--- rpm-server.py
+++ rpm-server.py
@@ -2,10 +2,10 @@
 from os import chdir
 from http.server import HTTPServer, CGIHTTPRequestHandler
 # this is the folder where you need to store your files to be served.
-files='./data'
+files='./files'
 chdir(files)
 # This is the port you want to listen for requests
-port=8083
+port=80
 server_object = HTTPServer(server_address=('',port),
 RequestHandlerClass=CGIHTTPRequestHandler)
 print('Starting server on port ', port)
